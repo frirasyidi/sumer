01efd6155314c7b5f98112f4b720098577c30ca1
Fri <fri@Fris-MacBook-Air.local>
2023-10-05 12:21:49 +0800

    Add test with many numbers

--- src/sumer.test.ts
+++ src/sumer.test.ts
@@ -10,4 +10,9 @@ describe('sumer will add given numbers together',()=>{
     const result = sumer(40,37)
     expect(result).toBe(77)
   })
+
+  it('should add many numbers together', ()=>{
+    const result = sumer(40,37,-8,29,1_100,394,-820,5,234,-2,1,0,-49,98,903)
+    expect(result).toBe(1_962)
+  })
 })
