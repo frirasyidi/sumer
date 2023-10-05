fe34b00dd67768eee46eaa62a0cd5de929654987
Fri <fri@Fris-MacBook-Air.local>
2023-10-05 16:12:29 +0800

    Add ability to change number to object

--- src/objectify.test.ts
+++ src/objectify.test.ts
@@ -5,4 +5,9 @@ describe("objectify will turn number and array to object", () => {
     const result = objectify({ "0": 0, "1": 1, "2": 2 });
     expect(result).toEqual({ "0": 0, "1": 1, "2": 2 });
   });
+
+  it("should change a number to object", () => {
+    const result = objectify(8);
+    expect(result).toEqual({ "0": 8 });
+  });
 });
--- src/objectify.ts
+++ src/objectify.ts
@@ -1,11 +1,12 @@
 /**
  * Turn number and array to object when necessary. Will do nothing given an object.
  * @param item number | number[]
- * @returns number[]
+ * @returns an object with string as key and number as value
  */
-function objectify(item: { [key: string]: number }): {
+function objectify(item: number | { [key: string]: number }): {
   [key: string]: number;
 } {
+  if (typeof item === "number") return { "0": item };
   return item;
 }
 
