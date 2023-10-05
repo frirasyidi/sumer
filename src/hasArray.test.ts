eb4f74210e2f412c80106e25fbef1ac74ecee10a
Fri <fri@Fris-MacBook-Air.local>
2023-10-05 12:55:50 +0800

    Add test to check if the hasArray helper function return false when no array is provided

--- src/hasArray.test.ts
+++ src/hasArray.test.ts
@@ -5,4 +5,9 @@ describe("hasArray will check a given array has an array in it", () => {
     const result = hasArray(1, 2, [3, 4], 5);
     expect(result).toBe(true);
   });
+
+  it("should return false when there is no array in it", () => {
+    const result = hasArray(1, 2, 3, 4, 5);
+    expect(result).toBe(false);
+  });
 });
